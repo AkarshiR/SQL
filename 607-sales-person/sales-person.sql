b1e698685d9af2289ecdd759a5b47539e7770281
Akarshi rajput <akarshirajput123@gmail.com>
2026-03-07 16:24:12 +0530

    Time: 1342 ms (61.54%) | Memory: 0B (100.00%) - LeetSync

---
+++ 607-sales-person/sales-person.sql
@@ -0,0 +1,7 @@
+# Write your MySQL query statement below
+SELECT sp.name
+FROM SalesPerson sp
+LEFT JOIN Orders o 
+    ON sp.sales_id = o.sales_id
+    AND o.com_id = (SELECT com_id FROM Company WHERE name = 'RED')
+WHERE o.order_id IS NULL;
